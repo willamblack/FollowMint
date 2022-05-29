8cd5108943a105466a2ff5434dd2a9e9d025c06a
Fooyao <fooyao@163.com>
2022-05-29 15:42:44 +0800

    Update FollowMint.py
    增加监控时间配置

--- FollowMint.py
+++ FollowMint.py
@@ -1,4 +1,6 @@
+import ctypes
 import json
+import sys
 import time
 import requests
 from blocknative.stream import Stream
@@ -12,27 +14,51 @@ configExample = {
     "barkKey": "",
     "maxGasPrice": 50,
     "maxGasLimit": 1000000,
-    "follow": [
-        "0x8888887a5e2491fec904d90044e6cd6c69f1e71c",
-        "0x555555B63d1C3A8c09FB109d2c80464685Ee042B",
-        "0x99999983c70de9543cdc11dB5DE66A457d241e8B"
-    ]
+    "follow": {
+        "0x8888887a5e2491fec904d90044e6cd6c69f1e71c":{"start": 18, "end": 6},
+        "0x555555B63d1C3A8c09FB109d2c80464685Ee042B":{"start": 0, "end": 24},
+        "0x99999983c70de9543cdc11dB5DE66A457d241e8B":{"start": 8, "end": 20}
+    }
 }
+std_out_handle = ctypes.windll.kernel32.GetStdHandle(-11)
+
+
+def set_cmd_text_color(color, handle=std_out_handle):
+    Bool = ctypes.windll.kernel32.SetConsoleTextAttribute(handle, color)
+    return Bool
+
+
+# reset white
+def resetColor():
+    set_cmd_text_color(0x0c | 0x0a | 0x09)
 
 
 def print_green(message):
     stime = time.strftime("%m-%d %H:%M:%S", time.localtime())
-    print(f'[{stime}] \033[1;32m{message}\033[0m')
+    set_cmd_text_color(0x0a)
+    sys.stdout.write(f'[{stime}] {message}\n')
+    resetColor()
 
 
 def print_red(message):
     stime = time.strftime("%m-%d %H:%M:%S", time.localtime())
-    print(f'[{stime}] \033[1;31m{message}\033[0m')
+    set_cmd_text_color(0x0d)
+    sys.stdout.write(f'[{stime}] {message}\n')
+    resetColor()
 
 
 def print_blue(message):
     stime = time.strftime("%m-%d %H:%M:%S", time.localtime())
-    print(f'[{stime}] \033[1;34m{message}\033[0m')
+    set_cmd_text_color(0x0b)
+    sys.stdout.write(f'[{stime}] {message}\n')
+    resetColor()
+
+
+def print_yellow(message):
+    stime = time.strftime("%m-%d %H:%M:%S", time.localtime())
+    set_cmd_text_color(0x0e)
+    sys.stdout.write(f'[{stime}] {message}\n')
+    resetColor()
 
 
 def bark(info, data):
@@ -47,7 +73,7 @@ def getMethodName(methodSignature):
         res = requests.get('https://www.4byte.directory/api/v1/signatures/?hex_signature=' + methodSignature)
         if res.status_code == 200:
             methodName = res.json()['results'][0]['text_signature'].split('(')[0].lower()
-            print_blue(res.json()['results'][0]['text_signature'])
+            print_yellow(res.json()['results'][0]['text_signature'])
             if 'mint' in methodName:
                 methodNameList[methodSignature] = True
                 return True
@@ -65,18 +91,26 @@ async def txn_handler(txn, unsubscribe):
     gasPrice = int(gasPrice)
     inputData = txn['input']
     value = txn['value']
+    for follow in follows:
+        if follow.lower() == from_address.lower():
+            starttime = int(follows[follow]['start'])
+            endtime = int(follows[follow]['end'])
+            tm_hour = time.localtime().tm_hour
+            if tm_hour < starttime or tm_hour > endtime:
+                print_yellow("非Mint时间，跳过")
+                return
     if value != '0':
-        print_blue("非免费，跳过")
+        print_yellow("非免费，跳过")
         return
     if to_address in mintadd:
-        print_blue("mint过，跳过")
+        print_yellow("mint过，跳过")
         return
     inputData = inputData.replace(from_address[2:].lower(), account.address[2:].lower())
     if not getMethodName(inputData[:10]):
-        print_blue('可能不是mint交易,跳过')
+        print_yellow('可能不是mint交易,跳过')
         return
     if gasPrice > maxGasPrice:
-        print_blue('gasPrice过高,跳过')
+        print_yellow('gasPrice过高,跳过')
         return
     transaction = {
         'from': account.address,
@@ -90,13 +124,13 @@ async def txn_handler(txn, unsubscribe):
     try:
         estimateGas = w3.eth.estimateGas(transaction)
         if estimateGas > maxGasLimit:
-            print_blue('超过gasLimit上限，跳过')
+            print_yellow('超过gasLimit上限，跳过')
             return
         transaction['gas'] = estimateGas
         signed = w3.eth.account.sign_transaction(transaction, privateKey)
         new_raw = signed.rawTransaction.hex()
         if to_address in mintadd:
-            print_blue("mint过，跳过")
+            print_yellow("mint过，跳过")
             return
         tx_hash = w3.eth.sendRawTransaction(new_raw)
         mintadd.append(to_address)
@@ -109,7 +143,7 @@ async def txn_handler(txn, unsubscribe):
             print_green("mint失败")
             bark('mint失败', 'https://cn.etherscan.com/tx/' + w3.toHex(tx_hash))
     except:
-        print_blue('预测失败，跳过')
+        print_yellow('预测失败，跳过')
         return
 
 
@@ -142,15 +176,26 @@ if __name__ == '__main__':
         blocknativeKey = config['blocknativeKey']
         barkKey = config['barkKey']
         follows = config['follow']
-        w3 = Web3(Web3.HTTPProvider(RPC))
-        maxGasPrice = config['maxGasPrice']
-        maxGasPrice = w3.toWei(maxGasPrice, 'gwei')
-        maxGasLimit = int(config['maxGasLimit'])
-        chainId = w3.eth.chainId
-        account = w3.eth.account.privateKeyToAccount(privateKey)
-        mintadd = []
-        methodNameList = {}
-        main()
+        if type(follows) == list:
+            print_red('请重新配置follow起始时间，配置文件模板已更新')
+            followsDict = {}
+            for follow in follows:
+                followsDict[follow] = {'start': 0, 'end': 24}
+            config['follow'] = followsDict
+            file = open('config.json', 'w')
+            file.write(json.dumps(config))
+            file.close()
+            time.sleep(10)
+        else:
+            w3 = Web3(Web3.HTTPProvider(RPC))
+            maxGasPrice = config['maxGasPrice']
+            maxGasPrice = w3.toWei(maxGasPrice, 'gwei')
+            maxGasLimit = int(config['maxGasLimit'])
+            chainId = w3.eth.chainId
+            account = w3.eth.account.privateKeyToAccount(privateKey)
+            mintadd = []
+            methodNameList = {}
+            main()
     except Exception as e:
         print_red(str(e))
         time.sleep(10)
